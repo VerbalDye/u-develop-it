dfb6b5c228c45b5f1a01fd5da9bd0dac1a69f0de
Dustin Huntsberger <dhunts258@gmail.com>
2022-05-02 17:24:48 -0400

    added more express.js code

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'SQLtest2022#',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 app.use((req, res) => {
     res.status(404).end();
